b2cc0492bd4d38cbf0a5d0b0a40536db36f180c3
Daniel Hodgson <99420927+dwhodgson@users.noreply.github.com>
2022-02-11 11:38:43 +0000

    Add files via upload
    Updated docs to include favicon.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,8 @@ extensions = [
     'numpydoc',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.imgmath'
+    'sphinx.ext.imgmath',
+    'sphinx-favicon'
 ]
 autosummary_generate = True
 
@@ -56,4 +57,8 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', '../_static']
+
+favicons = {
+    'static-file': 'favicon.png'
+}
